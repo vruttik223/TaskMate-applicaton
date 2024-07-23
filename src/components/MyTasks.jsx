5a6c90cc9d6c02a81445cadf3c589a2a409a6c95
vruttik223 <163909247+vruttik223@users.noreply.github.com>
2024-07-24 00:19:50 +0530

    Update MyTasks.jsx

--- src/components/MyTasks.jsx
+++ src/components/MyTasks.jsx
@@ -14,7 +14,7 @@ function MyTasks() {
   }, []);
 
   function getData() {
-    fetch( "https://github.com/vruttik223/TaskMate-applicaton/blob/main/src/utils/data.json/data/").then((result) => {
+    fetch( "https://vruttik223.github.io/TaskMate-applicaton/data/").then((result) => {
       result.json().then((res) => {
         setData(res);
       });
@@ -24,7 +24,7 @@ function MyTasks() {
   function submitTask() {
     let taskInfo = { title, description, dueDate, priority };
 
-    fetch( "https://github.com/vruttik223/TaskMate-applicaton/blob/main/src/utils/data.json/data/", {
+    fetch( "https://vruttik223.github.io/TaskMate-applicaton/data/", {
       method: "POST",
       headers: {
         "Accept": "application/json",
@@ -48,7 +48,7 @@ function MyTasks() {
   }
 
   function deleteTask(id) {
-    fetch(`https://github.com/vruttik223/TaskMate-applicaton/blob/main/src/utils/data.json/data/${id}`, {
+    fetch(`https://vruttik223.github.io/TaskMate-applicaton/data/${id}`, {
       method: "DELETE"
     }).then((result) => {
       result.json().then((res) => {
@@ -72,7 +72,7 @@ function MyTasks() {
 
   function updateTask() {
     let updatedTask = { title, description, dueDate, priority, id };
-    fetch(`https://github.com/vruttik223/TaskMate-applicaton/blob/main/src/utils/data.json/data/${id}`, {
+    fetch(`https://vruttik223.github.io/TaskMate-applicaton/data/${id}`, {
       method: "PUT",
       headers: {
         "Accept": "application/json",
